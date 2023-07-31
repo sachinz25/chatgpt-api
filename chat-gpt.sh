781b3960c4da7ee2ee1ed63afa2530459e5df903
Sachin Zade <114475206+sachinz25@users.noreply.github.com>
2023-08-01 03:21:30 +0530

    Update chat-gpt.sh

--- chat-gpt.sh
+++ chat-gpt.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Prompt the user for their API token
+read -p "Enter your ChatGPT API token: " API_TOKEN
+
+# Prompt the user for their ChatGPT prompt
+read -p "Enter your ChatGPT prompt: " PROMPT
+
+# Make the API call to ChatGPT using the user's API token and prompt
+curl -H "Authorization: Bearer $API_TOKEN"        
+     -d '{"prompt":"'$PROMPT'",
+           "userID":"anon",
+           "maxTokens":150}'       
+     https://api.chatgpt.com/v1/openai/complete
